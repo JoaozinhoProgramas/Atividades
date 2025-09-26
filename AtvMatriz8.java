aa8e2dad39194fc53eeeeb91dd06902c170e17ab
JoaozinhoProgramas <joao90510245@edu.unipar.br>
2025-09-25 22:07:02 -0300

    Add files via upload

--- AtvMatriz8.java
+++ AtvMatriz8.java
@@ -1,82 +1,36 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-
-public class AtvMatriz7 {
-    
-    public static void main (String[] args) {
-        char X = 'X';
-        char O = 'O';
-        char [][] matriz = new char[3][3];
-        Scanner sc = new Scanner(System.in);
-        int Escolha = 0;
-        int rodadas = 0;
-        
-        boolean vitoria = false;
-        
-        System.out.println("==========================");
-        System.out.println("=*****JOGO DA VELHA******=");
-        System.out.println("==========================");
-        System.out.println("Escolha o jogador que começa:");
-        System.out.println("1 - Jogador X");
-        System.out.println("2 - Jogador O");
-        Escolha = sc.nextInt();
-        
-    
-        if (Escolha == 1) {
-            System.out.println("\n  0 1 2");
-            for (int i = 0; i < 3; i++) {
-                System.out.print(i + " ");
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(matriz[i][j]);
-                }
-                System.out.println();
-            }
-        }
-        System.out.println();
-    
-            
-            for (int i = 0; i < 3; i ++) {
-                for (int j = 0; j < 3; j ++) {
-                    System.out.println("Digite sua escolha, Linha e Coluna");
-                    rodadas += 1;
-                    int Linha = sc.nextInt();
-                    int Coluna = sc.nextInt();
-                    matriz[i][j] = O;
-                        if (Linha >= 0 && Linha < 3 && Coluna >= 0 && Coluna < 3)  {
-                        matriz[Linha][Coluna] = X;
-                        System.out.println(Arrays.deepToString(matriz));
-                        }
-                    
-                }
-            // validação de vitoria
-            if (matriz[0][0] == X && matriz[1][1] == X && matriz[2][2] == X) {
-                vitoria = true;
-                System.out.println("Você venceu parabens!!");
-                break;
-            }
-            if (matriz[0][2] == X && matriz[1][1] == X && matriz[2][0] == X) {
-                vitoria = true;
-                System.out.println("Você venceu parabens!!");
-                break;
-            }
-            if (matriz[0][0] == X && matriz[0][1] == X && matriz[0][2] == X) {
-                vitoria = true;
-                System.out.println("Você venceu parabens!!");
-                break;
-            }
-            if (matriz[1][0] == X && matriz[1][1] == X && matriz[1][2] == X) {
-                vitoria = true;
-                System.out.println("Você venceu parabens!!");
-                break;
-            }
-            if (matriz[2][0] == X && matriz[2][1] == X && matriz[2][2] == X) {
-                vitoria = true;
-                System.out.println("Você venceu parabens!!");
-                break;
-            }
-        }
-    }
+public class AtvMatriz8
+{
+	public static void main(String[] args) {
+		double matriz[][] = new double[5][2];
+		
+		double nota1 = 0;
+		double nota2 = 0;
+		double media = 0;
+		double mediaDividida = 0;
+		
+		int index = -1;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 5; i++)  {
+		    index = i + 1;
+		    
+		    System.out.println("Digite 1º nota do aluno");
+		    nota1 = sc.nextInt();
+		    matriz[i][0] = nota1;
+		    
+		    System.out.println("Digite 2º nota do aluno");
+		    nota2 = sc.nextInt();
+		    matriz[i][1] = nota2;
+		    
+		    media = nota1 + nota2;
+		    mediaDividida = media / 2.0;
+		    
+		    System.out.println("O " + index + "º aluno tem as seguintes médias:" + mediaDividida);  
+		}
+		System.out.println("Banco de dados das notas:" + Arrays.deepToString(matriz));
+	}
 }
-
-   
